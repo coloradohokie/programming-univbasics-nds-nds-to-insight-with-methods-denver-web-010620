d870b4a434bc9d1dca7fd7802080f72d10d67d6a
Michael Newman <coloradohokie@gmail.com>
2019-12-16 23:37:21 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -7,10 +7,28 @@ require 'directors_database'
 
 def directors_totals(nds)
   result = {}
-  nil
+  director_count = 0 #list of directors; first Array
+  while director_count < nds.length do
+    #get the name of each director
+    director = nds[director_count][:name] 
+
+    #get the gross of all of the movies they made by calling function
+    gross = gross_for_director(nds[director_count]) 
+
+    result[director] = gross
+    director_count += 1
+  end
+  return result
 end
 
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
+  gross = 0    
+  movie_count = 0 #list of each movies for each director; second array
+  while movie_count < director_data[:movies].length do
+    gross += director_data[:movies][movie_count][:worldwide_gross]
+    movie_count += 1
+  end
+  return gross
 end
